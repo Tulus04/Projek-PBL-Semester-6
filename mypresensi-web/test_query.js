5f1c5c9ae86f296b7f5d106716dc7eb5d1fc2ea1
Tulus04 <arzitul@gmail.com>
2026-06-10 23:12:16 +0800

    chore: remove test script

--- mypresensi-web/test_query.js
+++
@@ -1,4 +0,0 @@
-require('dotenv').config({path: '.env.local'});
-const { createClient } = require('@supabase/supabase-js');
-const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.SUPABASE_SERVICE_ROLE_KEY);
-supabase.from('sessions').select('id, course_id, courses(code, name)').limit(1).then(res => console.log(JSON.stringify(res, null, 2)));
